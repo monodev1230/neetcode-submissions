73ebd40cd026f4417b884c0fbee8431e301ae3e6
monodev1230 <86416082+monodev1230@users.noreply.github.com>
2026-08-04 16:27:16 -0600

    Add: house-robber - submission-0

---
+++ Data Structures & Algorithms/house-robber/submission-0.py
@@ -0,0 +1,11 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        maxProfit = 0
+        n = len(nums)
+        dp = [0] * n
+        dp[0] = nums[0]
+        for i in range(1, n):
+            twoBefore = dp[i-2] if i > 1 else 0
+            threeBefore = dp[i-3] if i > 2 else 0
+            dp[i] = max(dp[i-1], nums[i] + twoBefore, nums[i] + threeBefore)
+        return dp[n-1]
