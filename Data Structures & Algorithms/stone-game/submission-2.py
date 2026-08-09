8f0b18af6dba013b4a9f98635bf6b9433b8976c2
monodev1230 <86416082+monodev1230@users.noreply.github.com>
2026-08-08 20:19:28 -0600

    Add: stone-game - submission-2

---
+++ Data Structures & Algorithms/stone-game/submission-2.py
@@ -0,0 +1,30 @@
+class Solution:
+    def stoneGame(self, piles: List[int]) -> bool:
+        n = len(piles)
+        alice = 0
+        aliceChoices = []
+        bobChoices = []
+        bob = 0
+        isAlice = True
+        left, right = 0, n - 1
+        for _ in range(n):
+            leftValue = piles[left] - piles[left+1] if left + 1 < n else 0
+            rightValue = piles[right] - piles[right-1] if right - 1 >= 0 else 0
+            if leftValue > rightValue:  
+                if isAlice:
+                    alice += piles[left]
+                    aliceChoices.append(piles[left])
+                else:
+                    bob += piles[left]
+                    bobChoices.append(piles[left])
+                left += 1
+            else:
+                if isAlice:
+                    alice += piles[right]
+                    aliceChoices.append(piles[right])
+                else:
+                    bob += piles[right]
+                    bobChoices.append(piles[right])
+                right -= 1
+            isAlice = not isAlice
+        return True
