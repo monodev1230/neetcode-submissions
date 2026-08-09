13a518bfa587e6133ae423240bf03fd0c2aa45e9
monodev1230 <86416082+monodev1230@users.noreply.github.com>
2026-08-09 17:56:24 -0600

    Add: edit-distance - submission-2

---
+++ Data Structures & Algorithms/edit-distance/submission-2.py
@@ -0,0 +1,20 @@
+from functools import cache
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        m = len(word1)
+        n = len(word2)
+
+        @cache
+        def dfs(i, j):
+            if i == m:
+                return n - j
+            if j == n:
+                return m - i
+            res = 0
+            if word1[i] == word2[j]:
+                return dfs(i+1, j+1)
+
+            res = min(dfs(i+1,j), dfs(i, j+1))
+            res = min(res, dfs(i+1, j+1))
+            return res + 1
+        return dfs(0, 0)
