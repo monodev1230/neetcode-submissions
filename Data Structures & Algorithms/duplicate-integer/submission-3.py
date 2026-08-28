4e7d0ab65d2dfa64b71720e9694bdc719b247386
monodev1230 <86416082+monodev1230@users.noreply.github.com>
2026-08-27 21:27:30 -0600

    Add: duplicate-integer - submission-3

---
+++ Data Structures & Algorithms/duplicate-integer/submission-3.py
@@ -0,0 +1,8 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        memo = {}
+        for num in nums:
+            if num in memo:
+                return True
+            memo[num] = 1
+        return False
